577b8768efb0b5e3e3cfe68758ca6fadcbe172d2
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-28 12:11:17 +0330

    feat: next-auth installed + authOptions and authentication handler function implemented

---
+++ utils/authOptions.js
@@ -0,0 +1,32 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // Invoked on successful singin
+    async signIn({ profile }) {
+      // 1. connect to database
+      // 2. check if user exist
+      // 3. if not, then add user to database
+      // 4. return true to allow sign in
+    },
+    // Modifies the session object
+    async session({ session }) {
+      // 1. get user from database
+      // 2. assign the user id to the session
+      // 3.return session
+    },
+  },
+};
