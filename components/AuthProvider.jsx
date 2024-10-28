d0c20d39ed28b8ef61bc5f7e3fc0f6bc6d5ec574
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-28 12:42:30 +0330

    fix: session provider import fixed

--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -1,5 +1,5 @@
 "use client";
-import SessionProvider from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
