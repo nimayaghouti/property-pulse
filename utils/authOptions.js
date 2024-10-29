32d3721cfde5bbd1abc85838222573cf4bd09c0a
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-29 14:17:11 +0330

    feat: sign in functionality and adding user to database implemented

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,15 +20,30 @@ export const authOptions = {
     // Invoked on successful singin
     async signIn({ profile }) {
       // 1. connect to database
+      await connectDB();
       // 2. check if user exist
+      const userExists = await User.findOne({ email: profile.email });
       // 3. if not, then add user to database
+      if (!userExists) {
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. return true to allow sign in
+      return true;
     },
     // Modifies the session object
     async session({ session }) {
       // 1. get user from database
+      const user = await User.findOne({ email: session.user.email });
       // 2. assign the user id to the session
+      session.user.id = user._id.toString();
       // 3.return session
+      return session;
     },
   },
 };
